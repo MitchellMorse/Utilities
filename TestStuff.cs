de04b9ebc65e8426fa8f1b4e44bd57b65f2fecc9
MitchellMorse <mitchellmorse008@gmail.com>
2016-06-16 20:26:42 -0600

    Learned getters and setters mess up unity serialization

--- TestStuff.cs
+++ TestStuff.cs
@@ -2,14 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Models
 {
     public class TestStuff
     {
-        public int Id { get; set; }
-        public string TestData { get; set; }
-        public string NewString { get; set; }
+        public int Id;
+        public string TestData;
     }
 }
